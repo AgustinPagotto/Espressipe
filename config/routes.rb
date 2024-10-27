30fc7e65b9463f6356f5bb4341761dca8f656542
AgustinPagotto <agustinpagotto@gmail.com>
2024-10-27 17:58:40 -0300

    login with session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show create update destroy]
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
 end
