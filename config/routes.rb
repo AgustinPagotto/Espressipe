984960a130cf206e3ef858ccf98b6a3b422dff29
AgustinPagotto <agustinpagotto@gmail.com>
2024-10-27 18:15:17 -0300

    Revert "login with session"
    This reverts commit 30fc7e65b9463f6356f5bb4341761dca8f656542.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show create update destroy]
-  get '/login', to: 'session#new'
-  post '/login', to: 'session#create'
-  delete '/logout', to: 'session#destroy'
 end
