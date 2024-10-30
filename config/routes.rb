c1b366b217654040253c7d6198600b39eca2a712
AgustinPagotto <agustinpagotto@gmail.com>
2024-10-30 12:42:25 -0300

    brew controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :brews, only: %i[index show create update destroy]
   resources :users, only: %i[create update destroy]
   post '/auth/login', to: 'auth#login'
   get 'me', to: 'users#me'
