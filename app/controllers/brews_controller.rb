1c017b21eaa79ee5501c1d5c700b9d167d355f3b
AgustinPagotto <agustinpagotto@gmail.com>
2024-10-30 17:10:03 -0300

    brew controller and model modifications

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -1,16 +1,32 @@
 class BrewsController < ApplicationController
+  before_action :authorized_current_user, only: %i[update destroy]
+
   def index
+    render json: current_user.brews, status: :ok
   end
 
   def show
+    render json: current_user.brews.find(params[:id]), status: :ok
   end
 
   def create
+    brew = current_user.brews.build(brew_params)
+    if brew.save
+      render json: brew, status: :created
+    else
+      render json: { errors: brew.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def brew_params
+    params.permit(:cofamount, :watamount, :method, :grindsetting, :temperature, :date, :extrationtime, :ratingbrew, :ratingtaste)
   end
 end
--- app/models/brew.rb
+++ app/models/brew.rb
@@ -1,3 +1,15 @@
 class Brew < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+
+  validates :user_id, presence: true
+  validates :cofamount, presence: true, numericality: { only_integer: true }
+  validates :watamount, presence: true, numericality: { only_integer: true }
+  validates :method, presence: true, length: { maximum: 30 }
+  validates :grindsetting, numericality: { only_integer: true }, allow_nil: true
+  validates :temperature, numericality: { only_integer: true }, allow_nil: true
+  validates :date, allow_nil: true
+  validates :extrationtime, numericality: { only_integer: true }, allow_nil: true
+  validates :ratingbrew, numericality: { only_integer: true }, allow_nil: true
+  validates :ratingtaste, numericality: { only_integer: true }, allow_nil: true
+
 end
